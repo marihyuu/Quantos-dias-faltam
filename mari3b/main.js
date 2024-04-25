7ed660d7d2daa67b412699c5d3cb7c8bbded4bbd
Marih <108813737+marihyuu@users.noreply.github.com>
2024-04-25 16:29:19 -0300

    Add files via upload

---
+++ mari3b/main.js
@@ -0,0 +1,16 @@
+@import url('https://fonts.googleapis.com/css2?family-Chakra+Petch: wght@480;700&display= Swap');
+:root{
+--cor-de-fundo: #1E1E1E:
+--Verde: #6FFF57;
+-- branco: #FFFFFF:
+body {
+background-color:var(cor-de-fundo);
+color: var(--branco )
+font-family: Chakra Petch sansserif;
+text-align: centens
+hl span{
+color: var(--verde)
+#dias restantes{
+color: var(--verde)
+font-size: 50px;
+}
